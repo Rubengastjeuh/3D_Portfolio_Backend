1ea50e7ccaf409da72cd2a4fc34dc7427211f160
Ruben <ruben.vanpoucke@student.odisee.be>
2024-01-22 17:31:51 +0100

    testing cors

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'https://deno.land/x/hono/mod.ts'
-import { cors } from 'https://deno.land/x/hono/middleware.ts' // Import the cors middleware
+import { cors } from 'https://deno.land/x/hono/middleware.ts'
 
 import ProjectController from "./controllers/ProjectController.ts";
 import PersonalInfoController from "./controllers/PersonalInfoController.ts";
@@ -14,10 +14,14 @@ const app = new Hono({ strict: false }).basePath("/api");
 app.use(
   '/api/*',
   cors({
-    origin: ['http://localhost:3000'],
+    origin: 'http://localhost:3000',
+    allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests'],
+    allowMethods: ["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
+    exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+    maxAge: 600,
+    credentials: true,
   })
 )
-
 app.route("/projects", ProjectController);
 app.route("/personalInfo", PersonalInfoController);
 app.route("/contactInfo", ContactInfoController);
