cc1f450f3968bf1a7c6ba6eccdb18023c3bc81e9
Ruben <ruben.vanpoucke@student.odisee.be>
2024-01-22 17:34:01 +0100

    cors errorrrrr

--- main.ts
+++ main.ts
@@ -11,17 +11,7 @@ import { loadData } from "./dataLoader.ts";
 const app = new Hono({ strict: false }).basePath("/api");
 
 // Enable CORS for all routes under /api/*
-app.use(
-  '/api/*',
-  cors({
-    origin: 'http://localhost:3000',
-    allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests'],
-    allowMethods: ["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
-    exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
-    maxAge: 600,
-    credentials: true,
-  })
-)
+app.use('/api/*', cors({ origin: '*' }));
 app.route("/projects", ProjectController);
 app.route("/personalInfo", PersonalInfoController);
 app.route("/contactInfo", ContactInfoController);
