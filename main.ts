3b67eb28159accb851f840c1f20de0f0940cc6be
Ruben <ruben.vanpoucke@student.odisee.be>
2024-01-22 17:11:31 +0100

    testing cors

--- main.ts
+++ main.ts
@@ -1,11 +1,16 @@
 import { Hono } from "https://deno.land/x/hono@v3.11.8/mod.ts";
-import ProjectController from "./controllers/ProjectController.ts"
+import ProjectController from "./controllers/ProjectController.ts";
 import PersonalInfoController from "./controllers/PersonalInfoController.ts";
 import ContactInfoController from "./controllers/ContactInfoController.ts";
 import StudyController from "./controllers/StudyController.ts";
 import SkillController from "./controllers/SkillController.ts";
-import {loadData} from "./dataLoader.ts";
+import { loadData } from "./dataLoader.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts"; // Import the oakCors middleware
+
 const app = new Hono({ strict: false }).basePath("/api");
+
+// Enable CORS for all routes
+app.use(oakCors({ origin: "*" })); // Replace "*" with the specific origins you want to allow
 
 app.route("/projects", ProjectController);
 app.route("/personalInfo", PersonalInfoController);
